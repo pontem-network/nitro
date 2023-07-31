657330cdadc5aa41ed25591dcf41bd791024b711
vladimirovmm <vladimirov.m.m@mail.ru>
2023-07-31 17:26:10 +0500

    Checking the "solc" version

--- test-infra/src/solc.rs
+++ test-infra/src/solc.rs
@@ -1,8 +1,9 @@
-use anyhow::Result;
-
-async fn version_solc() -> Result<String> {
-    todo!()
-}
+use anyhow::{anyhow, bail, ensure, Result};
+use log::{debug, error};
+use regex::Regex;
+use semver::{Version, VersionReq};
+use std::str::FromStr;
+use web3::helpers::to_string;
 
 /// Version solc "0.8.19" is expected
 ///
@@ -16,5 +17,55 @@ async fn version_solc() -> Result<String> {
 /// Error: Error checking the "solc" version or unsuitable "solc" version
 ///
 pub async fn check_solc() -> Result<()> {
-    todo!()
+    let exp = "=0.8.19";
+    let req = VersionReq::parse(exp)?;
+
+    let version = version_solc().await?;
+
+    ensure!(
+        req.matches(&version),
+        "Version {exp:?} is expected. The current version is {version:?}"
+    );
+
+    Ok(())
+}
+
+async fn version_solc() -> Result<Version> {
+    debug!("$ solc --version");
+
+    let result = tokio::process::Command::new("solc")
+        .arg("--version")
+        .output()
+        .await?;
+
+    let stderr = String::from_utf8(result.stderr)?;
+    let stdout = String::from_utf8(result.stdout)?;
+
+    if !stderr.is_empty() {
+        error!("{stderr}");
+    }
+
+    { result.status.success() && !stdout.is_empty() }
+        .then_some(&stdout)
+        .and_then(|stdout| stdout.split_once("Version:"))
+        .map(|(_, version)| version.trim())
+        .ok_or_else(|| {
+            debug!("{stdout}");
+            anyhow!("Couldn't get the \"solc\" version")
+        })
+        .and_then(string_to_version)
+}
+
+fn string_to_version(source: &str) -> Result<Version> {
+    debug!("Parse string: {source}");
+
+    let rg = Regex::new(r"^(.*[^d])?(?<version>(\d+\.){2}\d+)([^d].*)?$")?;
+    let version_str = rg
+        .captures(source)
+        .and_then(|f| f.name("version"))
+        .map(|version| version.as_str().trim())
+        .ok_or_else(|| anyhow!("Not found"))?;
+    debug!("Version: {version_str}");
+
+    Ok(Version::from_str(&version_str)?)
 }
--- test-infra/tests/local_nitro_sol.rs
+++ test-infra/tests/local_nitro_sol.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, ensure, Result};
 
 use test_infra::check_service::wait_up;
+use test_infra::solc::check_solc;
 
 const DEFAULT_REST_IP: &str = "127.0.0.1";
 const DEFAULT_REST_PORT: u16 = 8551;
@@ -9,7 +10,9 @@ const DEFAULT_REST_PORT: u16 = 8551;
 async fn main() -> Result<()> {
     env_logger::builder().is_test(true).try_init()?;
 
+    // pre
     assert!(wait_up(&format!("http://{DEFAULT_REST_IP}:{DEFAULT_REST_PORT}")).await);
+    check_solc().await.unwrap();
 
     Ok(())
 }
